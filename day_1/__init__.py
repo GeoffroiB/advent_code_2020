c2c1a3d69cb1a7b865cadbf6ce02f891e7002f0f
GeoffroiB <geoffroi.boulanger.1@ens.etsmtl.ca>
2020-12-01 01:07:04 -0500

    Fixed day 1 one liners v2.

--- day_1/__init__.py
+++ day_1/__init__.py
@@ -9,7 +9,7 @@ def day_1_1():
     for comb in combinations(lines, 2):
         if np.sum(comb) == 2020:
             print(np.prod(comb))
-    # [print(np.prod(comb)) for comb in combinations([int(i) for i in open("day_1/data.txt").readlines()[:-1]], 2) if np.sum(prod) == 2020]
+    # [print([np.prod(comb) for comb in combinations([int(i) for i in open("day_1/data.txt").readlines()[:-1]], 2) if np.sum(comb) == 2020])]
 
 
 def day_1_2():
@@ -19,4 +19,4 @@ def day_1_2():
     for comb in combinations(lines, 3):
         if np.sum(comb) == 2020:
             print(np.prod(comb))
-    # [print(np.prod(comb)) for comb in combinations([int(i) for i in open("day_1/data.txt").readlines()[:-1]], 3) if np.sum(prod) == 2020]
+    # [print([np.prod(comb) for comb in combinations([int(i) for i in open("day_1/data.txt").readlines()[:-1]], 3) if np.sum(comb) == 2020])]
