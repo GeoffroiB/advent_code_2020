ce5e43ccb75e277c77f9df74313938d218ab3ae3
GeoffroiB <geoffroi.boulanger.1@ens.etsmtl.ca>
2020-12-01 00:50:39 -0500

    Cleaned up day 1

--- day_1/__init__.py
+++ day_1/__init__.py
@@ -1,21 +1,22 @@
 from itertools import combinations
+import numpy as np
 
 
 def day_1_1():
     lines = []
     with open("day_1/data.txt") as f:
-        lines = f.readlines()[:-1]
-    for x, y in combinations(lines, 2):
-        if int(x) + int(y) == 2020:
-            print(int(x) * int(y))
-    # [print(int(x)*int(y)) for x, y in combinations(open("day_1/data.txt").readlines()[:-1], 2) if int(x)+int(y) == 2020]
+        lines = [int(i) for i in f.readlines()[:-1]]
+    for comb in combinations(lines, 2):
+        if np.sum(comb) == 2020:
+            print(np.prod(comb))
+    # [print(np.prod(comb)) for comb in combinations(open("day_1/data.txt").readlines()[:-1], 2) if np.sum(prod) == 2020]
 
 
 def day_1_2():
     lines = []
     with open("day_1/data.txt") as f:
-        lines = f.readlines()[:-1]
-    for x, y, z in combinations(lines, 3):
-        if int(x) + int(y) + int(z) == 2020:
-            print(int(x) * int(y) * int(z))
-    # [print(int(x)*int(y)*int(z)) for x, y, z in combinations(open("day_1/data.txt").readlines()[:-1], 3) if int(x)+int(y) == 2020]
+        lines = [int(i) for i in f.readlines()[:-1]]
+    for comb in combinations(lines, 3):
+        if np.sum(comb) == 2020:
+            print(np.prod(comb))
+    # [print(np.prod(comb)) for comb in combinations(open("day_1/data.txt").readlines()[:-1], 3) if np.sum(prod) == 2020]
