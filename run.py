2a157a97707f2a41ff7e245ccd217f19fc870cff
GeoffroiB <geoffroi.boulanger.1@ens.etsmtl.ca>
2020-12-09 17:44:18 -0500

    Run : Generalized running procedure to allow for multiple.

--- run.py
+++ run.py
@@ -5,11 +5,13 @@ import days
 
 
 def main(args: List[str]):
-    day = getattr(days, f"Day{int(args[0])}")()
-    day.run()
+    for arg in args:
+        day_number = int(arg)  # may fail if not parsable
+        day = getattr(days, f"Day{day_number}")()
+        day.run()
 
 
-DAY_NUMBER: int = 3  # default value, if not in arguments
+DAY_NUMBER: int = 6  # default value, if not in arguments
 
 if __name__ == "__main__":
     main(sys.argv[1:] or [str(DAY_NUMBER)])
