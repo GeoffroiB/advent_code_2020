20dc274664b8c33838e0c59d8885e47035ad3066
GeoffroiB <geoffroi.boulanger.1@ens.etsmtl.ca>
2020-12-10 14:37:49 -0500

    Run : Simplified logic when not supplying argument for day number.

--- run.py
+++ run.py
@@ -5,13 +5,14 @@ import days
 
 
 def main(args: List[str]):
+    if not args:
+        args = [int(input("Enter day to execute: "))]
+
     for arg in args:
         day_number = int(arg)  # may fail if not parsable
         day = getattr(days, f"Day{day_number}")()
         day.run()
 
 
-DAY_NUMBER: int = 6  # default value, if not in arguments
-
 if __name__ == "__main__":
-    main(sys.argv[1:] or [str(DAY_NUMBER)])
+    main(sys.argv[1:])
