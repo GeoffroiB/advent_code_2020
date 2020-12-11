4ecc91f5a88858765482d72e4003919a6162a4ec
GeoffroiB <geoffroi.boulanger.1@ens.etsmtl.ca>
2020-12-11 01:03:59 -0500

    Day 11 : Small optimizations and format refactoring.

--- days/day_11.py
+++ days/day_11.py
@@ -12,8 +12,6 @@ class Day11(AbstractDay):
         state_1 = [[c for c in line] for line in read_lines(self.inputs_path)]
         height, width = len(state_1), len(state_1[0])
 
-        state_2 = [["." for i in range(width)] for j in range(height)]
-
         def count_around(x, y):
             count = 0
             for a in range(x-1, x+2):
@@ -25,18 +23,22 @@ class Day11(AbstractDay):
                     count += (state_1[a][b] == "#")
             return count
 
+        state_2 = deepcopy(state_1)
+
         while True:
             # print("\n".join(["".join(row) for row in state_1]), end="\n\n")
             for i, row in enumerate(state_1):
                 for j, cell in enumerate(row):
                     n_count = count_around(i, j)
 
-                    if cell == "L" and n_count == 0:
-                        state_2[i][j] = "#"
-                    elif cell == "#" and n_count >= 4:
-                        state_2[i][j] = "L"
-                    else:
-                        state_2[i][j] = cell
+                    if cell == ".":
+                        continue
+                    elif cell == "L":
+                        if n_count == 0:
+                            state_2[i][j] = "#"
+                    elif cell == "#":
+                        if n_count >= 4:
+                            state_2[i][j] = "L"
 
             for r1, r2 in zip(state_1, state_2):
                 if "".join(r1) != "".join(r2):
@@ -69,17 +71,15 @@ class Day11(AbstractDay):
                         count += 1
                     elif state_1[a][b] == ".":
                         dx, dy = a-x, b-y  # [-1, 0, 1]
-                        a2 = a + dx
-                        b2 = b + dy
-                        while True:
+                        a2, b2 = a, b
+
+                        while state_1[a2][b2] != ".":
+                            a2 += dx
+                            b2 += dy
                             if a2 not in range(height) or b2 not in range(width):
                                 break
-                            elif state_1[a2][b2] != ".":
-                                count += (state_1[a2][b2] == "#")
-                                break
-                            else:
-                                a2 += dx
-                                b2 += dy
+                        else:
+                            count += (state_1[a2][b2] == "#")
 
             return count
 
