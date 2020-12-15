b007559bc195c79ca1301008c866ad7fe42e60fe
GeoffroiB <geoffroi.boulanger.1@ens.etsmtl.ca>
2020-12-15 16:47:07 -0500

    Day 13 : Vastly improved efficiency.

--- days/day_13.py
+++ days/day_13.py
@@ -1,68 +1,74 @@
 # -*- coding: utf-8 -*-
 
-from itertools import combinations
 import numpy as np
-import numba
+from typing import List, Tuple
 
 from .abstract_day import AbstractDay
-from .utils import read_ints, read_lines, product
+from .utils import read_lines
+
+int64 = np.int64
+uint64 = np.uint64
 
 
 class Day13(AbstractDay):
-    def partOne(self) -> int:
-        lines = read_lines(self.inputs_path)
-        target_t = int(lines[0])
-        tt = [int(i) for i in lines[1].split(",") if i != "x"]
+    def partOne(self) -> uint64:
+        target_value, divisors = Day13.parseForPartOne(self.inputs_path)
 
-        closest, closest_distance = -1, 10000
+        closest_divisor: uint64 = divisors.pop(0)
+        least_distance: uint64 = closest_divisor - (target_value % closest_divisor)
 
-        for t in tt:
-            ta = ((target_t // t) + 1) * t
-            dist = ta - target_t
-            if dist < closest_distance:
-                closest = t
-                closest_distance = dist
+        for divisor in divisors:
+            distance: uint64 = divisor - (target_value % divisor)
+            if distance < least_distance:
+                closest_divisor = divisor
+                least_distance = distance
 
-        return closest * closest_distance
+        # print(f"{closest_divisor=} {least_distance=}")
 
-    def partTwo(self) -> int:
-        values = []
-        skips = []
-        lines = read_lines(self.inputs_path)
+        return closest_divisor * least_distance
 
-        for vv in lines[1].split(","):
-            if vv == "x":
-                skips[-1] += 1
-            else:
-                values.append(int(vv))
-                skips.append(1)
+    @staticmethod
+    def parseForPartOne(file_path: str) -> Tuple[uint64, List[uint64]]:
+        lines = read_lines(file_path)
+        target_value = uint64(lines[0])
+        values = [uint64(e) for e in lines[1].split(",") if e != "x"]
+        return target_value, values
 
-        if skips[-1] != 0:
-            skips[-1] = 0
-        print(f"{skips=}")
-        print(
-f"""
-#include <iostream>
-#include <cstdint>
+    def partTwo(self) -> int64:
+        entries: List[Tuple[int64, int64]] = Day13.parseForPartTwo(self.inputs_path)  # List[(divisor, remainder)]
 
-int main() {{
-\tuint64_t target{{{values[0]}}};
-\tfor (uint64_t i{{ target }}; i < 18446744073709551615-target+1ull; i += target) {{
-"""
+        step_counter = np.uint64(0)
+        cummul_divisor, cummul_remainder = entries.pop(-1)
+        while entries:
+            n_divisor, n_remainder = cummul_divisor, cummul_remainder
+            m_divisor, m_remainder = entries.pop(-1)
+
+            diff_remainder_n_m = np.int64(n_remainder - m_remainder)
+
+            n_candidate = int64(0)
+            while n_candidate < n_divisor:  # On average, faster than a ranged loop
+                if int64((diff_remainder_n_m + n_candidate * n_divisor) % m_divisor) == 0:
+                    break
+                n_candidate += 1
+
+            net_remainder = np.int64(n_remainder + n_candidate * n_divisor)
+            net_divisor = np.int64(n_divisor * m_divisor)
+
+            cummul_divisor, cummul_remainder = (net_divisor, net_remainder)
+
+            step_counter += n_candidate
+
+        # print(f"{step_counter=}")
+
+        return cummul_remainder
+
+    @staticmethod
+    def parseForPartTwo(file_path: str) -> List[Tuple[int64, int64]]:
+        """
+        Returns pairs (divisor, remainder) for each entry in file
+        """
+        raw_entries: List[str] = (read_lines(file_path)[1]).split(",")
+        return sorted(
+            [(int64(number), int64(-bus_id)) for bus_id, number in enumerate(raw_entries) if number != "x"],
+            key=lambda entry: entry[0]  # Sorted ascendingly by divisor
         )
-
-        string = "i"
-        tab = '\t'
-        for i, (v, s) in enumerate(zip(values[1:], skips[:-1])):
-            string += f" + {s}"
-            print(f"{tab*(i+2)}if ((({string}) % {v}) == 0) {{")
-
-        print(f"{tab * (len(values) + 1)}std::cout << i << std::endl;")
-        print(f"{tab * (len(values) + 1)}break;")
-
-        for i in range(len(values)-1)[::-1]:
-            print(f"{tab * (i + 2)}}}")
-        print("\t}")
-
-        print("\tstd::cin.get();")
-        print("}")
